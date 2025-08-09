5fb7f8a89421a87267f94cc8fb303b02a4555be6
Cosmin-Mihai Florescu <94063560+cosmf@users.noreply.github.com>
2025-08-09 20:15:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
-
+var config = {
+    lines: [
+        "Dragă Irene,",
+        "Mi-ai dat lumea peste cap. Nu știu când s-au scurs atâtea zile, dar de la primul moment când te-am invitat la o cafea mi-am dat seama că tu ești specială pentru mine.",
+        "Am mers împreună pe un drum cu tot felul de gropi și ocolișuri — copiat la SCD, restanța la ASC, chiar era să mor, tăiat cu hârtie, când învățam împreună pentru EGC. Din toate astea nu ne-am dezvoltat doar profesional, ci am descoperit și că facem o echipă minunată — nu doar pe plan academic, ci pe toate planurile.",
+        "Ești o fată isteață foc, frumoasă și descurcăreață, talentată în tot ceea ce faci, lângă care știu că nu m-aș putea plictisi niciodată. Ai o fire calmă, calculată și echilibrată, iar eu sunt agitat și aventuros — ca două note diferite din aceeași melodie. Ești o persoană caldă si atentă și, cel mai important, o prietenă aproape la nevoie.",
+        "Poate că uneori am reacționat mai stângaci sau nepotrivit, dar îmi doresc să ajungem din nou în acele zile când conversațiile noastre păreau fără sfârșit.", 
+    ],
+    date: "2023-11-12T21:05:00+02:00",
+    names: ["☃︎ Al tău Cosmin, ",""]
+}
