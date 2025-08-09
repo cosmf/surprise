0f69f11d9827ff22d7bd0e53944cf849483db1bd
Cosmin-Mihai Florescu <94063560+cosmf@users.noreply.github.com>
2025-08-09 18:18:32 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+    lines: [
+        "Draga Irenush,",
+        "Iti scriu aceasta scrisoare, pentru a iti relata, cat de speciala esti pentru mine,  prin faptul ca mi-ai dat lumea peste cap din momentul in care am inceput sa vorbim. Nu prea stiu cand s-au scurs atatea zile, de la primul moment cand te-am invitat la o cafea, pana acum, dar stiu ca nu a trecut o zi in care sa nu ma gandesc la tine.",
+        "Ce trece timpul, nu? :)",
+        "Am trecut noi prin multe, ne-am luat copiat la scd, ne-am luat restanta la asc, l-am avut pe cap pe muistul de la app, chiar era sa mor, taiat cu hartie, cand invatam pentru egc. Dar am reusit sa trecem impreuna peste toate, si as spune chiar ca facem o echipa buna, acum ingineri oficial :D, nu numai pe plan academic, ci pe toate planurile.",
+        "Esti o fata isteata foc, frumoasa, descurcareata, talentata in tot ceea ce faci, langa care simt ca nu m-as putea plictisi niciodata. Esti o persoana blanda, grijulie si cel mai important, o prietena aproape la nevoie, iar eu asta am vrut sa iti arat, ca pot sa fiu un prieten de incredere, un om in care poti sa iti pui baza. Am invatat de la tine cum sa fiu un om mai calm, ordonat si cel mai important, sa privesc viata asa cum este, si cu bune si cu rele.", 
+        "Imi pare rau daca nu am stiut mereu sa fiu asa cum meritai, daca uneori am reactionat stangaci sau nepotrivit. Sper din tot sufletul ca, in curand, lucrurile sa se aseze si sa ne intoarcem la zilele acelea in care nu ne puteam opri din vorbit zi si noapte.  ",
+    ],
+    date: "2023-11-12T21:05:00+02:00",
+    names: ["☃︎ Al tau Michael, ",""]
+}
